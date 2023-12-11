8fdd00407a1789fefe01e7faae3a2926183d39f9
thisis-arman <armanhossen1549@gmail.com>
2023-12-11 23:59:40 +0600

    hh

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,14 +1,7 @@
 import express from 'express';
-import { UserControllers } from './user.controller';
-import validateRequest from '../../utils/validateRequest';
-import { studentValidationSchema } from '../student/student.validation';
 
 const router = express.Router();
 
-router.post(
-  '/create-student',
-  validateRequest(studentValidationSchema),
-  UserControllers.createStudent,
-);
+router.post('/create-academic-semester');
 
-export const UserRoutes = router;
+export const academicSemesterRoutes = router;
