52392df6b0581ffd611acb09eb3e823202ebb2f1
thisis-arman <armanhossen1549@gmail.com>
2023-12-18 20:27:47 +0600

    route setup for academic faculty

---
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { academicFacultyValidations } from './academicFaculty.validation';
+import { AcademicFacultyController } from './academicFaculty.controller';
+
+const router = express.Router();
+
+router.post(
+  '/create-student',
+  validateRequest(academicFacultyValidations.createAcademicFacultyValidation),
+  AcademicFacultyController.createAcademicFaculty,
+);
+
+router.get('/', AcademicFacultyController.getAcademicFaculties);
+
+router.get('/:facultyId', AcademicFacultyController.getSingleAcademicFaculty);
+
+router.patch('/:facultyId', AcademicFacultyController.updateAcademicFaculty);
+export const AcademicFacultyRoutes = router;
